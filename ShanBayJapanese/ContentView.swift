7e3b741b11776c7da245a9688e2c97882cf9e02c
NorthPoleCat <598114329@qq.com>
2026-09-08 21:19:05 +0800

    Initial commit

---
+++ ShanBayJapanese/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShanBayJapanese
+//
+//  Created by xun liu on 2026/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShanBayJapanese/ShanBayJapaneseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShanBayJapaneseApp.swift
+//  ShanBayJapanese
+//
+//  Created by xun liu on 2026/9/7.
+//
+
+import SwiftUI
+
+@main
+struct ShanBayJapaneseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
